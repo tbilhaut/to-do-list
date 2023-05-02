da3ddec1ef1bc138dc8c469aa9033ad5e09c7e71
tbilhaut <bilhaut.th@gmail.com>
2023-05-02 16:43:54 +0200

    commit oublié par théo ce génie

---
+++ deleteApi.js
@@ -0,0 +1,16 @@
+// Récupération du bouton "Delete All"
+const deleteAllBtn = document.querySelector(".delete-all");
+
+// Récupération de la liste des tâches
+const tasksList = document.querySelector(".delete");
+
+// Ajout d'un gestionnaire d'événement sur le clic du bouton "Delete All"
+deleteAllBtn.addEventListener("click", function () {
+    // Suppression de toutes les tâches de la liste
+    //
+
+
+
+    
+
+});
